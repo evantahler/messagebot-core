fcc78d69d937431930a2969c4697ca8ba424a1ec
Evan Tahler <evantahler@gmail.com>
2016-04-04 13:18:29 -0700

    check for wget

--- db/maxmind/download.sh
+++ db/maxmind/download.sh
@@ -4,5 +4,12 @@ cd "$(dirname "$0")"
 cd ../..
 mkdir -p ./tmp/maxmind
 cd ./tmp/maxmind
+
+condition=$(which wget 2>/dev/null | grep -v "not found" | wc -l)
+if [ $condition -eq 0 ] ; then
+    echo "wget is not installed"
+    exit 1
+fi
+
 wget http://geolite.maxmind.com/download/geoip/database/GeoLiteCity.dat.gz
 gunzip GeoLiteCity.dat.gz -f
