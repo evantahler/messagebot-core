8cecfb2215f04c8925d477ddd4d9314d6f45fad2
Evan Tahler <evantahler@gmail.com>
2016-04-13 14:52:35 -0700

    now you can search properly

--- db/elasticsearch/indexes/messages.js
+++ db/elasticsearch/indexes/messages.js
@@ -1,7 +1,17 @@
 module.exports = {
   "settings": {
     "number_of_shards": parseInt(process.env.NUMBER_OF_SHARDS || 10),
-    "number_of_replicas": parseInt(process.env.NUMBER_OF_REPLICAS || 0)
+    "number_of_replicas": parseInt(process.env.NUMBER_OF_REPLICAS || 0),
+    "index":{
+        "analysis":{
+           "analyzer":{
+              "analyzer_keyword":{
+                 "tokenizer":"keyword",
+                 "filter":"lowercase"
+              }
+           }
+        }
+     }
   },
 
   "mappings": {
@@ -13,12 +23,12 @@ module.exports = {
             "match_mapping_type": "string",
             "mapping": {
               "type": "string",
-              "index": "not_analyzed",
+              "analyzer":"analyzer_keyword",
             }
           }
         }
       ],
-      
+
       "properties": {
         "guid":       { "type": "string" },
 
