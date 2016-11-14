be263a6cc4328ccaffee9ddb5ee7a7f997048046
Evan Tahler <evantahler@gmail.com>
2016-11-14 15:44:23 -0800

    remove assets

--- initializers/navigation.js
+++ initializers/navigation.js
@@ -18,8 +18,8 @@ module.exports = {
       glyphicon: 'info-sign',
       loggedIn: false,
       elements: [
-        {route: '/#/home', title: 'Home', glyphicon: 'user', highlights: ['^\/home$']},
-        {route: '/#/about', title: 'About', glyphicon: 'info-sign', highlights: ['^\/about$']},
+        {route: '/', title: 'Home', glyphicon: 'user', highlights: ['^\/$']},
+        {route: '/about', title: 'About', glyphicon: 'info-sign', highlights: ['^\/about$']},
       ]
     });
 
@@ -31,17 +31,17 @@ module.exports = {
       glyphicon: 'equalizer',
       loggedIn: true,
       elements: [
-        {route: '/#/people/recent', title: 'People: Recent', glyphicon: 'user', highlights: ['^\/people\/recent.*$']},
-        {route: '/#/people/search', title: 'People: Search', glyphicon: 'user', highlights: ['^\/people\/search.*$']},
-        {route: '/#/person/new', title: 'Person: New', glyphicon: 'user', highlights: ['^\/person\/new$']},
+        {route: '/people/recent', title: 'People: Recent', glyphicon: 'user', highlights: ['^\/people\/recent.*$']},
+        {route: '/people/search', title: 'People: Search', glyphicon: 'user', highlights: ['^\/people\/search.*$']},
+        {route: '/person/new', title: 'Person: New', glyphicon: 'user', highlights: ['^\/person\/new$']},
         {divider: true},
-        {route: '/#/events/recent', title: 'Events: Recent', glyphicon: 'equalizer', highlights: ['^\/events\/recent.*$']},
-        {route: '/#/events/search', title: 'Events: Search', glyphicon: 'equalizer', highlights: ['^\/events\/search.*$']},
-        {route: '/#/event/new', title: 'Event: New', glyphicon: 'equalizer', highlights: ['^\/event\/new$']},
+        {route: '/events/recent', title: 'Events: Recent', glyphicon: 'equalizer', highlights: ['^\/events\/recent.*$']},
+        {route: '/events/search', title: 'Events: Search', glyphicon: 'equalizer', highlights: ['^\/events\/search.*$']},
+        {route: '/event/new', title: 'Event: New', glyphicon: 'equalizer', highlights: ['^\/event\/new$']},
         {divider: true},
-        {route: '/#/messages/recent', title: 'Messages: Recent', glyphicon: 'envelope', highlights: ['^\/messages\/recent.*$']},
-        {route: '/#/messages/search', title: 'Messages: Search', glyphicon: 'envelope', highlights: ['^\/messages\/search.*$']},
-        // {route: '/#/message/new', title: 'Message: New', glyphicon: 'envelope', highlights: ['^\/message\/new$']},
+        {route: '/messages/recent', title: 'Messages: Recent', glyphicon: 'envelope', highlights: ['^\/messages\/recent.*$']},
+        {route: '/messages/search', title: 'Messages: Search', glyphicon: 'envelope', highlights: ['^\/messages\/search.*$']},
+        // {route: '/message/new', title: 'Message: New', glyphicon: 'envelope', highlights: ['^\/message\/new$']},
       ]
     });
 
@@ -51,9 +51,9 @@ module.exports = {
       glyphicon: 'send',
       loggedIn: true,
       elements: [
-        {route: '/#/lists/list', title: 'Lists', glyphicon: 'folder-open', highlights: ['^\/lists\/list.*$', '^\/list\/.*$']},
-        {route: '/#/templates/list', title: 'Templates', glyphicon: 'file', highlights: ['^\/templates\/list.*$', '^\/template\/.*$']},
-        {route: '/#/campaigns/list', title: 'Campaigns', glyphicon: 'send', highlights: ['^\/campaigns\/list.*$', '^\/campaign\/.*$']},
+        {route: '/lists/list', title: 'Lists', glyphicon: 'folder-open', highlights: ['^\/lists\/list.*$', '^\/list\/.*$']},
+        {route: '/templates/list', title: 'Templates', glyphicon: 'file', highlights: ['^\/templates\/list.*$', '^\/template\/.*$']},
+        {route: '/campaigns/list', title: 'Campaigns', glyphicon: 'send', highlights: ['^\/campaigns\/list.*$', '^\/campaign\/.*$']},
       ]
     });
 
@@ -63,15 +63,15 @@ module.exports = {
       glyphicon: 'flash',
       loggedIn: true,
       elements: [
-        {route: '/#/status', title: 'Status', glyphicon: 'flash', highlights: ['^\/status$']},
+        {route: '/system/status', title: 'Status', glyphicon: 'flash', highlights: ['^\/status$']},
         {divider: true},
-        {route: '/#/settings', title: 'Settings', glyphicon: 'cog', highlights: ['^\/settings$']},
-        {route: '/swagger', title: 'API', glyphicon: 'cloud', highlights: null},
-        {route: '/resque', title: 'Resque', glyphicon: 'road', highlights: null},
+        {route: '/system/settings', title: 'Settings', glyphicon: 'cog', highlights: ['^\/settings$']},
+        // {route: '/swagger', title: 'API', glyphicon: 'cloud', highlights: null},
+        {route: '/system/resque/overview', title: 'Resque', glyphicon: 'road', highlights: null},
         {divider: true},
-        {route: '/#/account', title: 'Account', glyphicon: 'user', highlights: ['^\/account$']},
-        {route: '/#/users', title: 'Users', glyphicon: 'th', highlights: ['^\/users$']},
-        {route: '/#/logout', title: 'Log Out', glyphicon: 'off', highlights: ['^\/logout$']},
+        {route: '/user/edit', title: 'Account', glyphicon: 'user', highlights: ['^\/account$']},
+        {route: '/users/list', title: 'Users', glyphicon: 'th', highlights: ['^\/users$']},
+        {route: '/logout', title: 'Log Out', glyphicon: 'off', highlights: ['^\/logout$']},
       ]
     });
 
